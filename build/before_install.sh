43c929bb07098f8eb465aa5b8a93c656acaed13f
Sebastian Reimers <info@miete-admin.de>
2015-11-22 13:22:02 +0100

    fix lv2 dev

--- build/before_install.sh
+++ build/before_install.sh
@@ -2,7 +2,10 @@
 
 if [ "$1" == "linux" ]; then
     apt-get update -qq
-    apt-get install -y libssl-dev lv2-dev
+    apt-get install -y libssl-dev
+    wget http://lv2plug.in/spec/lv2-1.12.0.tar.bz2
+    tar xjf lv2-1.12.0.tar.bz2 
+    pushd lv2-1.12.0 && ./waf configure && ./waf build && sudo ./waf install && popd
 
     # remove libs to prefer static binding (*.a)
     rm -f /usr/lib/x86_64-linux-gnu/libcrypto.so
