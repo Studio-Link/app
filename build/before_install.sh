b3f685b4df24f758a42bd684f38e6df678ed16f9
Sebastian Reimers <info@miete-admin.de>
2015-11-22 13:13:56 +0100

    cleanup build and add lv2 plugin

--- build/before_install.sh
+++ build/before_install.sh
@@ -2,7 +2,9 @@
 
 if [ "$1" == "linux" ]; then
     apt-get update -qq
-    apt-get install -y libssl-dev
+    apt-get install -y libssl-dev lv2-dev
+
+    # remove libs to prefer static binding (*.a)
     rm -f /usr/lib/x86_64-linux-gnu/libcrypto.so
     rm -f /usr/lib/x86_64-linux-gnu/libssl.so
 fi
--- build/build.sh
+++ build/build.sh
@@ -6,34 +6,45 @@ opus="1.1"
 baresip="master"
 patch_url="https://github.com/Studio-Link-v2/baresip/compare/Studio-Link-v2:master"
 
+echo "start build on $TRAVIS_OS_NAME"
+
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     my_extra_lflags=""
 else
     my_extra_lflags="-framework SystemConfiguration -framework CoreFoundation"
 fi
 
-# Build libre ------
-wget -N "http://www.creytiv.com/pub/re-0.4.14.tar.gz"
+
+# Build libre
+#-----------------------------------------------------------------------------
+wget -N "http://www.creytiv.com/pub/re-${re}.tar.gz"
 tar -xzf re-${re}.tar.gz
 ln -s re-$re re
 cd re; make libre.a; cd ..
 
-# Build librem ------
-wget -N "http://www.creytiv.com/pub/rem-0.4.6.tar.gz"
+
+# Build librem
+#-----------------------------------------------------------------------------
+wget -N "http://www.creytiv.com/pub/rem-${rem}.tar.gz"
 tar -xzf rem-${rem}.tar.gz
 ln -s rem-$rem rem
 cd rem; make librem.a; cd ..
 
-# Build opus ------
-wget -N "http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz"
+
+# Build opus
+#-----------------------------------------------------------------------------
+wget -N "http://downloads.xiph.org/releases/opus/opus-${opus}.tar.gz"
 tar -xzf opus-${opus}.tar.gz
 cd opus-$opus; ./configure; make; cd ..
 mkdir opus; cp opus-$opus/.libs/libopus.a opus/
 mkdir -p my_include/opus
 cp opus-$opus/include/*.h my_include/opus/ 
 
+
 # Build baresip with studio link addons
+#-----------------------------------------------------------------------------
 git clone https://github.com/Studio-Link-v2/baresip.git baresip-master
+ln -s baresip-$baresip baresip
 cd baresip-$baresip;
 
 ## Add patches
@@ -41,11 +52,38 @@ curl ${patch_url}...studio-link-config.patch | patch -p1
 
 ## Link backend modules
 cp -a ../webapp modules/webapp
+cp -a ../effect modules/effect
 
 make LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 \
     MODULES="opus stdio ice g711 turn stun uuid webapp" \
     EXTRA_CFLAGS="-I ../my_include" EXTRA_LFLAGS="$my_extra_lflags -L ../opus"
+make LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 \
+    MODULES="opus stdio ice g711 turn stun uuid webapp effect" \
+    EXTRA_CFLAGS="-I ../my_include" EXTRA_LFLAGS="$my_extra_lflags -L ../opus" libbaresip.a
 cd ..
+
+
+# Build overlay-lv2 plugin (linux only)
+#-----------------------------------------------------------------------------
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    git clone https://github.com/Studio-Link-v2/overlay-lv2.git overlay-lv2
+    cp -a baresip/include/baresip.h overlay-lv2
+    mkdir overlay-lv2/re
+    cp -a re/include/* overlay-lv2/re/
+    cd overlay-lv2
+    ./build.sh
+    cd ..
+fi
+
+
+# Build overlay-audio-unit plugin (osx only)
+#-----------------------------------------------------------------------------
+# @TODO
+
+
+
+# Testing and prepare release upload
+#-----------------------------------------------------------------------------
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     ldd baresip-$baresip/baresip
