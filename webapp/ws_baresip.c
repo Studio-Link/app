c166f47b9ea99c5d8119fe7f0c13abdc2b017332
Sebastian Reimers <info@miete-admin.de>
2015-11-22 19:59:21 +0100

    cleanup

--- webapp/ws_baresip.c
+++ webapp/ws_baresip.c
@@ -47,49 +47,52 @@ void webapp_ws_baresip(const struct websock_hdr *hdr,
 
 	err = json_decode_odict(&cmd, 32, (const char *)mbuf_buf(mb),
 			mbuf_get_left(mb), 8);
-	if (!err) {
-		e = odict_lookup(cmd, "command");
-		if (e) {
-			if (!str_cmp(e->u.str, "accept")) {
-				ua_answer(uag_current(), NULL);
-			}
-			else if (!str_cmp(e->u.str, "hangup")) {
-				warning("close: %p", uag_current());
-				ua_hangup(uag_current(), NULL, 0, NULL);
-				if(!have_active_calls())
-					ws_send_all(WS_BARESIP, SIP_CLOSED);
-			}
-			else if (!str_cmp(e->u.str, "call")) {
-				e = odict_lookup(cmd, "dial");
-				if (!str_cmp(e->u.str, "")) {
-					ws_send_all(WS_BARESIP, SIP_EMPTY);
-				}
-				else {
-					ua_connect(uag_current(), NULL, NULL,
-							e->u.str, NULL, VIDMODE_ON);
-				}
-			}
-			else if (!str_cmp(e->u.str, "addsip")) {
-				webapp_account_add(e);
-				ws_send_json(WS_BARESIP, webapp_accounts_get());
-			}
-			else if (!str_cmp(e->u.str, "deletesip")) {
-				sip_delete(cmd, e);
-			}
-			else if (!str_cmp(e->u.str, "uagcurrent")) {
-				char aorfind[100] = {0};
+	if (err)
+		goto out;
 
-				e = odict_lookup(cmd, "aor");
-				if(e) {
-					snprintf(aorfind, sizeof(aorfind), "sip:%s", e->u.str);
-					uag_current_set(uag_find_aor(aorfind));
-					warning("Current Account: %s\n", ua_aor(uag_current()));
-					webapp_account_current();
-					ws_send_json(WS_BARESIP, webapp_accounts_get());
-				}
-			}
+	e = odict_lookup(cmd, "command");
+	if (!e)
+		goto out;
+
+	if (!str_cmp(e->u.str, "accept")) {
+		ua_answer(uag_current(), NULL);
+	}
+	else if (!str_cmp(e->u.str, "hangup")) {
+		warning("close: %p", uag_current());
+		ua_hangup(uag_current(), NULL, 0, NULL);
+		if (!have_active_calls())
+			ws_send_all(WS_BARESIP, SIP_CLOSED);
+	}
+	else if (!str_cmp(e->u.str, "call")) {
+		e = odict_lookup(cmd, "dial");
+		if (!str_cmp(e->u.str, "")) {
+			ws_send_all(WS_BARESIP, SIP_EMPTY);
+		}
+		else {
+			ua_connect(uag_current(), NULL, NULL,
+					e->u.str, NULL, VIDMODE_ON);
 		}
 	}
+	else if (!str_cmp(e->u.str, "addsip")) {
+		webapp_account_add(e);
+		ws_send_json(WS_BARESIP, webapp_accounts_get());
+	}
+	else if (!str_cmp(e->u.str, "deletesip")) {
+		sip_delete(cmd, e);
+	}
+	else if (!str_cmp(e->u.str, "uagcurrent")) {
+		char aorfind[100] = {0};
 
+		e = odict_lookup(cmd, "aor");
+		if (!e)
+			goto out;
+		snprintf(aorfind, sizeof(aorfind), "sip:%s", e->u.str);
+		uag_current_set(uag_find_aor(aorfind));
+		warning("Current Account: %s\n", ua_aor(uag_current()));
+		webapp_account_current();
+		ws_send_json(WS_BARESIP, webapp_accounts_get());
+	}
+
+out:
 	mem_deref(cmd);
 }
