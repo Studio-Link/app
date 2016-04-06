7f7f935dfb22234540bb9e13d28832d32762e08f
Sebastian Reimers <info@miete-admin.de>
2016-04-06 22:03:06 +0200

    jack dev files

--- build/before_install.sh
+++ build/before_install.sh
@@ -5,7 +5,7 @@ openssl="1.0.2g"
 if [ "$1" == "linux" ]; then
     if [ "$(id -u)" == "0" ]; then
         apt-get update -qq
-        apt-get install -y libasound2-dev
+        apt-get install -y libasound2-dev libjack-jackd2-dev
         wget http://lv2plug.in/spec/lv2-1.12.0.tar.bz2
         tar xjf lv2-1.12.0.tar.bz2 
         pushd lv2-1.12.0 && ./waf configure && ./waf build && sudo ./waf install && popd
