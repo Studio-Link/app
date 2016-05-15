2e4edca33ac58378cff2cbfd1ff42052c0f69b9a
Sebastian Reimers <info@miete-admin.de>
2016-05-15 13:15:36 +0200

    fix sed space on osx

--- build/build.sh
+++ build/build.sh
@@ -164,7 +164,7 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
         git clone \
             $github_org/overlay-audio-unit.git overlay-audio-unit
         cd overlay-audio-unit
-        sed -i'' s/SLVERSION_N/$version_n/ StudioLink/StudioLink.jucer
+        sed -i '' s/SLVERSION_N/$version_n/ StudioLink/StudioLink.jucer
         wget https://github.com/julianstorer/JUCE/archive/$juce.tar.gz
         tar -xzf $juce.tar.gz
         rm -Rf JUCE
@@ -185,7 +185,7 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
         cp -a my_include/baresip.h \
             overlay-standalone-osx/StudioLinkStandalone/
         cd overlay-standalone-osx
-        sed -i'' s/SLVERSION_N/$version_n/ StudioLinkStandalone/Info.plist
+        sed -i '' s/SLVERSION_N/$version_n/ StudioLinkStandalone/Info.plist
         ./build.sh
         cd ..
     fi
