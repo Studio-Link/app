8bc3c8c36345440629f62f4fdf7b3a745160b557
Sebastian Reimers <info@miete-admin.de>
2016-10-07 21:58:57 +0200

    increase keychain timeout

--- build/before_install.sh
+++ build/before_install.sh
@@ -16,5 +16,7 @@ elif [ "$1" == "osx" ]; then
         security import ./build/keychain/apple.cer -k ~/Library/Keychains/sl-build.keychain -T /usr/bin/codesign
         security import ./build/keychain/cert.cer -k ~/Library/Keychains/sl-build.keychain -T /usr/bin/codesign
         security import ./build/keychain/key.p12 -k ~/Library/Keychains/sl-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+        # Increase default timeout 5min
+        security set-keychain-settings -t 3600 -l ~/Library/Keychains/sl-build.keychain
     fi
 fi
