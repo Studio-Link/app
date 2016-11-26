8faa49a740e76b6326087da7c2e83567399c75df
Sebastian Reimers <info@miete-admin.de>
2016-11-26 10:20:14 +0100

    fix libre download prefix

--- build/build.sh
+++ build/build.sh
@@ -58,8 +58,8 @@ fi
 if [ ! -d re-$re ]; then
     wget -N "http://www.creytiv.com/pub/re-${re}.tar.gz"
     #wget https://github.com/creytiv/re/archive/${re}.tar.gz
-    tar -xzf ${re}.tar.gz
-    rm -f ${re}.tar.gz
+    tar -xzf re-${re}.tar.gz
+    rm -f re-${re}.tar.gz
     ln -s re-$re re
     cd re
     patch --ignore-whitespace -p1 < ../../build/patches/bluetooth_conflict.patch
