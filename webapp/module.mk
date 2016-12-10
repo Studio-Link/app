10362b5f3543d2ab0d5ed8aa5d95d42e30e1fc9e
Sebastian Reimers <info@miete-admin.de>
2016-12-10 23:09:20 +0100

    fix libsndfile

--- webapp/module.mk
+++ webapp/module.mk
@@ -10,6 +10,6 @@ $(MOD)_SRCS	+= mono.c record.c
 $(MOD)_SRCS	+= ws_baresip.c ws_contacts.c ws_chat.c ws_meter.c ws_calls.c
 $(MOD)_SRCS	+= ws_options.c
 $(MOD)_SRCS	+= websocket.c utils.c
-$(MOD)_LFLAGS   += -lm -lsndfile
+$(MOD)_LFLAGS   += -lm
 
 include mk/mod.mk
