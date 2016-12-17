4171e05ae36c93d6dea445b58950e4dacd00aaa7
Sebastian Reimers <info@miete-admin.de>
2016-12-17 10:35:31 +0100

    fix windows pthread.dll bug

--- build/build_windows.sh
+++ build/build_windows.sh
@@ -23,8 +23,6 @@ else
     _arch="x86_64-w64-mingw32"
 fi
 
-sudo mv /usr/i686-w64-mingw32/include/pthread.h /usr/i686-w64-mingw32/include/pthread.h_disabled
-
 # Download openssl
 #-----------------------------------------------------------------------------
 if [ ! -d openssl-${openssl} ]; then
