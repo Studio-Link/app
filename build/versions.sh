155b19a0648778deb95d9f439635e45097b6cd67
Sebastian Reimers <hallo@studio-link.de>
2018-01-16 13:10:39 +0100

    update baresip,libre,librem and increase version, refs #16

--- build/versions.sh
+++ build/versions.sh
@@ -1,11 +1,11 @@
-vmajor=17; vminor=9; vminor_t="09"; vpatch=0
+vmajor=18; vminor=2; vminor_t="02"; vpatch=0
 release="alpha"
-rem="0.5.1"
-re="0.5.5"
+rem="0.5.2"
+re="0.5.7"
 opus="1.2.1"
 openssl="1.1.0g"
 openssl_sha256="de4d501267da39310905cb6dc8c6121f7a2cad45a7707f76df828fe1b85073af"
-baresip="master"
+baresip="v18.02.0"
 juce="4.2.4"
 flac="1.3.2"
 github_org="https://github.com/Studio-Link-v2"
