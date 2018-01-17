b11fbef7e52666ea29b09e498604d9eeaed6b80c
Sebastian Reimers <hallo@studio-link.de>
2018-01-17 20:46:56 +0100

    fix shasum - the right way ;)

--- build/lib/functions.sh
+++ build/lib/functions.sh
@@ -1,16 +1,18 @@
-function sl_prepare {
+sl_prepare() {
     echo "start build on $TRAVIS_OS_NAME ($BUILD_OS)"
     vminor_t=$(printf "%02d" $vminor)
 
     mkdir -p src;
     pushd src
     mkdir -p my_include
+
+    SHASUM=$(which shasum)
 }
 
-function sl_get_openssl {
+sl_get_openssl() {
     wget https://www.openssl.org/source/openssl-${openssl}.tar.gz
     echo "$openssl_sha256  openssl-${openssl}.tar.gz" | \
-        which shasum -a 256 -c -
+        ${SHASUM} -a 256 -c -
     tar -xzf openssl-${openssl}.tar.gz
     ln -s openssl-${openssl} openssl
 }
