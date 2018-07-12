1f4fb73320cb1b10a59b0aac791e8b50f165d081
Sebastian Reimers <hallo@studio-link.de>
2018-07-12 10:06:14 +0200

    fix build debug tool helper

--- build/tools/build_debug.sh
+++ build/tools/build_debug.sh
@@ -1,8 +1,8 @@
 sl_extra_lflags="-L ../opus -L ../my_include "
-sl_extra_modules="alsa jack"
+sl_extra_modules="alsa rtaudio"
 
 make RELEASE=1 LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 \
     MODULES="opus stdio ice g711 turn stun uuid auloop webapp menu debug_cmd $sl_extra_modules" \
     EXTRA_CFLAGS="-I ../my_include" \
-    EXTRA_LFLAGS="$sl_extra_lflags"
+    EXTRA_LFLAGS="$sl_extra_lflags -L ../openssl"
 
