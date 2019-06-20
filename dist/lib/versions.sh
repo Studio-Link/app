ccb2bb4836c6d11829c495c7f0adcb81a18218ad
Sebastian Reimers <hallo@studio-link.de>
2019-06-20 14:50:05 +0200

    fix travis branch

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -14,4 +14,6 @@ flac="1.3.2"
 github_org="https://github.com/Studio-Link"
 vstsdk="vstsdk367_03_03_2017_build_352"
 debug="RELEASE=1"
+if [ -z "${TRAVIS_BRANCH+x}" ]; then
 TRAVIS_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+fi
