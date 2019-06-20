da36ca7695abd2b4b7bdbf62daad8bc1fe4758ac
Sebastian Reimers <hallo@studio-link.de>
2019-06-20 15:43:02 +0200

    bump version and update openssl

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,12 +1,12 @@
-vmajor=19; vminor=4; vpatch=0
+vmajor=19; vminor=6; vpatch=0
 vbuild="$(git rev-list HEAD --count).$(git rev-parse --short HEAD)"
 release="beta-${vbuild}"
 #release="beta"
 rem="0.6.0"
 re="0.6.0"
 opus="1.3.1"
-openssl="1.1.1b"
-openssl_sha256="5c557b023230413dfb0756f3137a13e6d726838ccd1430888ad15bfb2b43ea4b"
+openssl="1.1.1c"
+openssl_sha256="f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f4c90"
 rtaudio="master"
 baresip="0.6.0"
 juce="4.3.1"
