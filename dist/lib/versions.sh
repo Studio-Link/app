97d9f6d513b4276da9a8689bad8bcea498f9114f
Sebastian Reimers <hallo@studio-link.de>
2019-08-03 10:46:04 +0200

    bump version

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,14 +1,14 @@
-vmajor=19; vminor=6; vpatch=0
+vmajor=19; vminor=8; vpatch=0
 vbuild="$(git rev-list HEAD --count).$(git rev-parse --short HEAD)"
 release="beta-${vbuild}"
 #release="beta"
-rem="0.6.0"
+baresip="0.6.3"
 re="0.6.0"
+rem="0.6.0"
 opus="1.3.1"
 openssl="1.1.1c"
 openssl_sha256="f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f4c90"
 rtaudio="master"
-baresip="0.6.3"
 juce="4.3.1"
 flac="1.3.2"
 github_org="https://github.com/Studio-Link"
