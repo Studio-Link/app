fec4d75af77b8c11e3cd7b9ff375f12e4f0ab201
Sebastian Reimers <hallo@studio-link.de>
2019-09-01 21:50:48 +0200

    try to fix keychain

--- dist/before_build.sh
+++ dist/before_build.sh
@@ -15,7 +15,10 @@ elif [ "$BUILD_OS" == "osx" ]; then
         security create-keychain -p travis sl-build.keychain
         security import ./dist/keychain/apple.cer -k ~/Library/Keychains/sl-build.keychain -T /usr/bin/codesign
         security import ./dist/keychain/cert.cer -k ~/Library/Keychains/sl-build.keychain -T /usr/bin/codesign
-        security import ./dist/keychain/key.p12 -k ~/Library/Keychains/sl-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+        security import ./dist/keychain/key.p12 -k ~/Library/Keychains/sl-build.keychain -P $KEY_PASSWORD -A
+        security list-keychains -s ~/Library/Keychains/sl-build.keychain
+        security unlock-keychain -p $KEY_PASSWORD ~/Library/Keychains/sl-build.keychain
+
         # Increase default timeout 5min
         security set-keychain-settings -t 3600 -l ~/Library/Keychains/sl-build.keychain
     fi
