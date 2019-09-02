79c7e269d9e72f9c883e51a7211f81dd6984e7ef
Sebastian Reimers <hallo@studio-link.de>
2019-09-02 21:28:26 +0200

    remove debug_cmd from debug build

--- dist/tools/build_debug.sh
+++ dist/tools/build_debug.sh
@@ -2,7 +2,7 @@ sl_extra_lflags="-L ../opus -L ../my_include "
 sl_extra_modules="alsa slrtaudio"
 
 make RELEASE=1 LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 \
-    MODULES="opus stdio ice g711 turn stun uuid auloop webapp menu debug_cmd $sl_extra_modules" \
+    MODULES="opus stdio ice g711 turn stun uuid auloop webapp menu $sl_extra_modules" \
     EXTRA_CFLAGS="-I ../my_include" \
     EXTRA_LFLAGS="$sl_extra_lflags -L ../openssl"
 
