d095819a66b457c83f8e2f680a8915e6b2a0a0c4
Sebastian Reimers <hallo@studio-link.de>
2019-09-02 21:49:01 +0200

    fix some icons and update packages

--- src/webui/src/icons.js
+++ src/webui/src/icons.js
@@ -1,10 +1,17 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faCog } from '@fortawesome/free-solid-svg-icons'
-import { faRandom } from '@fortawesome/free-solid-svg-icons'
-import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
-import { faRss } from '@fortawesome/free-solid-svg-icons'
-import { faDotCircle } from '@fortawesome/free-regular-svg-icons'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faCog } from '@fortawesome/free-solid-svg-icons/faCog'
+import { faRandom } from '@fortawesome/free-solid-svg-icons/faRandom'
+import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons/faLongArrowAltRight'
+import { faRss } from '@fortawesome/free-solid-svg-icons/faRss'
+import { faDotCircle } from '@fortawesome/free-regular-svg-icons/faDotCircle'
+import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone'
+import { faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons/faMicrophoneSlash'
+import { faPhoneSlash } from '@fortawesome/free-solid-svg-icons/faPhoneSlash'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons/faUserPlus'
+import { faAddressBook } from '@fortawesome/free-solid-svg-icons/faAddressBook'
+import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons/faPhoneSquare'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons/faPlusCircle'
+
 
 // We are only using the user-astronaut icon
 library.add(faCog)
@@ -13,6 +20,12 @@ library.add(faLongArrowAltRight)
 library.add(faRss)
 library.add(faDotCircle)
 library.add(faPhone)
+library.add(faMicrophoneSlash)
+library.add(faPhoneSlash)
+library.add(faUserPlus)
+library.add(faAddressBook)
+library.add(faPhoneSquare)
+library.add(faPlusCircle)
 
 // Replace any existing <i> tags with <svg> and set up a MutationObserver to
 // continue doing this as the DOM changes.
