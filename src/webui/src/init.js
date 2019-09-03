bf16c1e279568faecfbbe3fbdfb3b6e9e246c633
Sebastian Reimers <hallo@studio-link.de>
2019-09-03 13:25:20 +0200

    fix webui

--- src/webui/src/init.js
+++ src/webui/src/init.js
@@ -13,5 +13,5 @@ window.bootbox = bootbox;
 var marked = require('marked');
 window.marked = marked;
 
-//window.ws_host = location.host;
-window.ws_host = "127.0.0.1:39242";
+window.ws_host = location.host;
+//window.ws_host = "127.0.0.1:39242";
