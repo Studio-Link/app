22a4cf4e0c79bc11a34a133f3bace9ee6dc2830d
Sebastian Reimers <hallo@studio-link.de>
2020-03-19 17:44:31 +0100

    fix ie11 javascript errors

--- src/webui/src/init.js
+++ src/webui/src/init.js
@@ -10,8 +10,5 @@ import Parsley from 'parsleyjs'
 var bootbox = require('bootbox');
 window.bootbox = bootbox;
 
-var marked = require('marked');
-window.marked = marked;
-
 window.ws_host = location.host;
 //window.ws_host = "127.0.0.1:39242";
