8108fe6f0fab9104c934926eee2d6f3a010476d9
Sebastian Reimers <hallo@studio-link.de>
2020-03-20 09:19:50 +0100

    release v20.03.6-rc0

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,7 +1,7 @@
 vmajor=20; vminor=03; vpatch=6
 vbuild="$(git rev-list HEAD --count).$(git rev-parse --short HEAD)"
-release="beta-${vbuild}"
-#release="stable"
+#release="beta-${vbuild}"
+release="rc0"
 baresip="20.03.1"
 baresip_lib="0.6.5"
 re="20.03.0"
