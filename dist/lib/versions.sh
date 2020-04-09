53de38517cb7066011b268b419ff54e02b7bc53d
Sebastian Reimers <hallo@studio-link.de>
2020-04-09 11:27:47 +0200

    bump version

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,7 +1,7 @@
 vmajor=20; vminor=04; vpatch=0
 vbuild="$(git rev-list HEAD --count).$(git rev-parse --short HEAD)"
-#release="beta3-${vbuild}"
-release="beta3"
+release="beta4-${vbuild}"
+#release="beta4"
 baresip="20.03.1"
 baresip_lib="0.6.5"
 re="20.03.0"
