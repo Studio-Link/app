3f748a6fff62ee377afe98b3b4b375e233bde804
Sebastian Reimers <hallo@studio-link.de>
2020-04-25 11:19:02 +0200

    debug: add ASAN option example

--- dist/tools/build_debug.sh
+++ dist/tools/build_debug.sh
@@ -32,4 +32,6 @@ make RELEASE=1 LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 \
     MODULES="opus stdio ice g711 g722 turn stun uuid auloop webapp menu $sl_extra_modules" \
     EXTRA_CFLAGS="-I ../my_include" \
     EXTRA_LFLAGS="$sl_extra_lflags -L ../openssl -fsanitize=address -fno-omit-frame-pointer"
+
+# ASAN_OPTIONS=fast_unwind_on_malloc=0 ./baresip
 fi
