06d38af91f81c52af837d7be5b776addc0e3ff0f
Sebastian Reimers <hallo@studio-link.de>
2020-05-24 10:03:59 +0200

    webapp: improve jitter buffer (less latency)

--- src/modules/webapp/jitter.c
+++ src/modules/webapp/jitter.c
@@ -7,7 +7,7 @@
 
 #define STIME 192 /* 192 = 48 Samples * 2ch * 2 Bytes = 1ms */
 
-static int16_t *dummy[3840];
+static int16_t *dummy[1920];
 
 /* src/audio.c */
 struct timestamp_recv {
@@ -57,11 +57,11 @@ void webapp_jitter(struct session *sess, int16_t *sampv,
 	size_t bufsz = 0;
 
 	/* set threshold to 500ms */
-	int16_t silence_threshold = 48000/sampc/2;
+	int16_t silence_threshold = 48000/sampc;
 
 	bufsz = aubuf_cur_size(rx->aubuf);
 
-	if (bufsz <= 1920*2*3 && !sess->talk) { /* <=60ms */
+	if (bufsz <= 7680 && !sess->talk) { /* >=40ms (1920*2*2) */
 		debug("webapp_jitter: increase latency %dms\n",
 				bufsz/STIME);
 		memset(sampv, 0, sampc * sizeof(int16_t));
@@ -95,10 +95,10 @@ void webapp_jitter(struct session *sess, int16_t *sampv,
 		return;
 
 	bufsz = aubuf_cur_size(rx->aubuf);
-	/* Reduce latency >= 160ms */
-	if (bufsz >= 1920*2*8 ) {
+	/* Reduce latency <= 120ms (1920*2*6) */
+	if (bufsz >= 23040) {
 		debug("webapp_jitter: reduce latency %dms\n",
 				bufsz/STIME);
-		wh(dummy, 3840, arg);
+		wh(dummy, 1920, arg);
 	}
 }
