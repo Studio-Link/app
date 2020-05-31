65c4a330dd6370c370514c63cbe5630eecf79630
Sebastian Reimers <hallo@studio-link.de>
2020-05-31 14:05:52 +0200

    release v20.05.0 stable

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,7 +1,7 @@
-vmajor=20; vminor=04; vpatch=5
+vmajor=20; vminor=05; vpatch=0
 vbuild="$(git rev-list HEAD --count).$(git rev-parse --short HEAD)"
-release="beta-${vbuild}"
-#release="beta"
+#release="beta-${vbuild}"
+release="stable"
 baresip="20.03.1"
 baresip_lib="0.6.5"
 re="20.03.0"
