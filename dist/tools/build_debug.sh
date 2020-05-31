85908b9162da67bb3444f95af88dfe2b1d4ad926
Sebastian Reimers <hallo@studio-link.de>
2020-05-31 07:45:53 +0200

    remove export

--- dist/tools/build_debug.sh
+++ dist/tools/build_debug.sh
@@ -1,8 +1,6 @@
 sl_extra_lflags="-L ../opus -L ../my_include "
 sl_extra_modules="alsa jack slaudio slogging dtls_srtp"
 
-export ASAN_OPTIONS="fast_unwind_on_malloc=0"
-
 if [ "$1" == "" ]; then
 export CC=gcc
 make RELEASE=1 LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 \
