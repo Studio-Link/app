b142f9117988eb8dc8595470fa8d0922027a2433
Sebastian Reimers <hallo@studio-link.de>
2020-06-06 11:55:39 +0000

    webui: rewrite onboarding/fix ie11 issues
    (cherry picked from commit 2b3c3e61ccab505703e547e9f03a17c856181896)

--- src/webui/src/init.js
+++ src/webui/src/init.js
@@ -1,6 +1,11 @@
 require('bootstrap');
 require('./icons');
 
+window.Vue = require("vue");
+import VueTour from 'vue-tour'
+
+require('vue-tour/dist/vue-tour.css')
+
 var $ = require('jquery');
 window.jQuery = $;
 window.$ = $;
@@ -10,5 +15,13 @@ import Parsley from 'parsleyjs'
 var bootbox = require('bootbox');
 window.bootbox = bootbox;
 
-window.ws_host = location.host;
-//window.ws_host = "127.0.0.1:8001";
+//window.ws_host = location.host;
+window.ws_host = "127.0.0.1:55301";
+
+Vue.use(VueTour);
+
+Vue.component("onboarding", require("./components/Onboarding.vue").default);
+
+const app = new Vue({
+    el: "#app"
+});
