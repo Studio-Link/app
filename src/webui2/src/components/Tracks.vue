9f1474a5263b5643ad162fe3df4d7084d62028a0
Sebastian Reimers <hallo@studio-link.de>
2020-08-31 21:32:40 +0200

    wip

--- src/webui2/src/components/Tracks.vue
+++ src/webui2/src/components/Tracks.vue
@@ -18,7 +18,8 @@
         @click="remoteTracks++; newTrackVisible = false"
         accesskey="t"
         aria-label="Add Remote Track"
-        class="inline-flex items-center rounded-lg px-20 py-12 text-sl-disabled font-bold text-2xl leading-none uppercase tracking-wide focus:outline-none"
+        :class="{'text-sl-disabled': newTrackVisible, 'text-sl-01dp': !newTrackVisible}"
+        class="inline-flex items-center rounded-lg px-20 py-12 font-bold text-2xl leading-none uppercase tracking-wide focus:outline-none focus:text-sl-disabled"
       >
         <svg
           aria-hidden="true"
@@ -31,7 +32,6 @@
           viewBox="0 0 448 512"
         >
           <path
-            :class="{'sm:invisible': !newTrackVisible}"
             fill="currentColor"
             d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
           />
