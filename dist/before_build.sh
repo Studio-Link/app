9dd3b071d5920dce29957f56787f79d277dd1e00
Sebastian Reimers <hallo@studio-link.de>
2020-12-24 09:55:20 +0100

    wip

--- dist/before_build.sh
+++ dist/before_build.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -e
 
 if [ "$BUILD_OS" == "linux" ]; then
-    id -u
-    if [ "$(id -u)" == "0" ]; then
-        apt-get update -qq
-        apt-get install -y libasound2-dev libjack-jackd2-dev libpulse-dev libpulse0 vim-common
+        sudo apt-get update -qq
+        sudo apt-get install -y libasound2-dev libjack-jackd2-dev libpulse-dev libpulse0 vim-common
         wget http://lv2plug.in/spec/lv2-1.14.0.tar.bz2
         tar xjf lv2-1.14.0.tar.bz2 
         pushd lv2-1.14.0 && ./waf configure && ./waf build && sudo ./waf install && popd
-    fi
 elif [ "$BUILD_OS" == "macos" ]; then
-    if [ "$(id -u)" != "0" ] && [ "$CIRCLECI" != "true" ]; then
-#        brew update
-#        brew install openssl
         security create-keychain -p travis sl-build.keychain
         security list-keychains -s ~/Library/Keychains/sl-build.keychain
         security unlock-keychain -p travis ~/Library/Keychains/sl-build.keychain
@@ -21,5 +15,4 @@ elif [ "$BUILD_OS" == "macos" ]; then
         #security import ./dist/keychain/cert.cer -k ~/Library/Keychains/sl-build.keychain -A
         security import ./dist/keychain/cert_2020.p12 -k ~/Library/Keychains/sl-build.keychain -P $KEY_PASSWORD_2020 -A
         security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k travis sl-build.keychain
-    fi
 fi
