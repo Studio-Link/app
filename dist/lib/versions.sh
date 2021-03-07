909c8a8092a1b378cdf7edd1fd135d38f836dffd
Sebastian Reimers <hallo@studio-link.de>
2021-03-07 09:30:22 +0100

    release v21.03.0-beta

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,14 +1,14 @@
-vmajor=21; vminor=1; vpatch=1
+vmajor=21; vminor=3; vpatch=0
 vbuild="$(git rev-parse --short HEAD)"
-release="beta-${vbuild}"
-#release="beta"
+#release="beta-${vbuild}"
+release="beta"
 baresip="1.0.0"
 baresip_lib="1.0.0"
 re="1.1.0"
 rem="0.6.0"
 sl3rdparty="v21.02.0"
 overlay="v19.09.0"
-juce="6.0.5"
+juce="6.0.7"
 github_org="https://github.com/Studio-Link"
 vstsdk="vstsdk367_03_03_2017_build_352"
 debug="RELEASE=1"
