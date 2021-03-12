8a5f4061a107b0c30b8c5648732511caa2fbfa50
Sebastian Reimers <hallo@studio-link.de>
2021-03-12 17:45:47 +0100

    release v21.03.1-beta

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,7 +1,7 @@
 vmajor=21; vminor=3; vpatch=1
 vbuild="$(git rev-parse --short HEAD)"
-release="beta-${vbuild}"
-#release="beta2"
+#release="beta-${vbuild}"
+release="beta"
 baresip="1.0.0"
 baresip_lib="1.0.0"
 re="1.1.0"
